13b3a1d362fcb6f224cd7a4a1071dc0302212dc1
Ryan Sousa <Sousa.Ryan4@gmail.com>
2020-01-04 15:09:19 -0800

    added the color coding function utilizing momentJS

--- script.js
+++ script.js
@@ -1,8 +1,36 @@
 $(document).ready(function() {
-  
-    $(".info").text("Whats on the agenda");
-  
-    var momentInterval = setInterval(function() {
-      $("#todaysDate").text(moment().format("MMMM Do YYYY, h:mm a"));
-    });
+  //record current hour for addColor
+  var current = moment().hour();
+  console.log(moment().hour());
+
+  //text
+  $(".info").text("Whats on the agenda");
+
+  //MomemtJS display
+  var momentInterval = setInterval(function() {
+    $("#todaysDate").text(moment().format("MMMM Do YYYY, h:mm a"));
+  });
+
+  //assign hour value to tr class
+  addColor(".time9", 9);
+  addColor(".time10", 10);
+  addColor(".time11", 11);
+  addColor(".time12", 12);
+  addColor(".time1", 13);
+  addColor(".time2", 14);
+  addColor(".time3", 15);
+  addColor(".time4", 16);
+  addColor(".time5", 17);
+
+  //function to add colors
+  function addColor(trNumber, time) {
+    if (current > time) {
+      $(trNumber).addClass("over");
+    } else if (current < time) {
+      $(trNumber).addClass("soon");
+    } else if ((current = time)) {
+      $(trNumber).addClass("now");
+    }
+    console.log(time);
+  }
 });
